84269d778593381bbf163dc6fa3e71578f3d79e3
John Runyard <jrunyard3ft@gmail.com>
2021-01-19 18:47:32 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// console.log("hello world")
+// console.log("hello world")
+
+window.app = new Vue({
+  el: "#appContainer",
+  data() {
+    return {
+      name: "",
+      num: 1234,
+      switch: false,
+      complicated: Object,
+    }
+  },
+  mounted() {
+    // console.log("Hello world")
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+})
