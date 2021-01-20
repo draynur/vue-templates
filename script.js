2cc944e8d1ae87cbd808ff5b8b24e3677a58ee60
John Runyard <jrunyard3ft@gmail.com>
2021-01-19 20:01:52 -0600

    Added VueJS template

--- script.js
+++ script.js
@@ -1,20 +1,68 @@
 // console.log("hello world")
 // console.log("hello world")
 
+// Template goes here
+Vue.component('does-nothing-at-all', {
+  template:`
+  <div>
+    <div> Hello-World  </div>
+    <div> {{customatt}}  </div>
+  </div>
+  `,
+  props: {
+    customatt: ""
+  },
+
+  data() {
+    return {
+
+    }
+  },
+
+  mounted() {
+    console.log(this.customatt)
+  },
+
+  methods: {
+    stuff() {
+
+    }
+  },
+
+  watch: {
+    customatt(prev, cur) {
+      console.log(prev, cur);
+    }
+  }
+
+})
+
+// Use the mix-in
+// Vue.use()
+
 window.app = new Vue({
   el: "#appContainer",
   data() {
     return {
-      name: "",
+      name: "stuff",
       num: 1234,
       switch: false,
       complicated: Object,
+      customString: "hello from custom att"
     }
   },
   mounted() {
-    // console.log("Hello world")
+    console.log("Hello world")
   },
-  methods: {},
-  computed: {},
+  methods: {
+    submit() {
+      console.log("I'm alive")
+    }
+  },
+  computed: {
+    changesSometimes() {
+      return this.name + this.num
+    }
+  },
   watch: {},
 })
